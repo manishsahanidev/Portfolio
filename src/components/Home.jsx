1653c8156f2dff81ef575c9edb734a6e52240fcc
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-02-01 11:12:18 +0530

    Modified: web logo + Resume download button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,17 +19,18 @@ const Home = () => {
           </p>
 
           <div>
-            <Link
-              to="portfolio"
-              smooth
-              duration={500}
+            <a
+              href="/ManishSahaniResume.pdf"
+              target="_blank"
+              rel='noreferrer'
+              download={true}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
-              Portfolio
+              Resume
               <span className="group-hover:rotate-90 duration-300">
                 <MdKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </Link>
+            </a>
           </div>
         </div>
         
