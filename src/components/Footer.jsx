242cef7293fba0c40f1197c704a0ea4580fe42e4
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-02-03 14:10:19 +0530

    modified: footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
     <footer className="lg:hidden flex flex-col items-center w-full h-fit bg-gradient-to-b from-gray-800 to-black p-4 text-white  border-t-4 border-gray-400">
       <div className="container pt-9">
         <div className="mb-5 ml-7 flex justify-center items-center">
-          <a href="https://github.com/manishsahanidev" className="mr-9 hover:scale-125 ">
+          <a href="https://github.com/manishsahanidev" target='_blank' className="mr-9 hover:scale-125 ">
             <FaGithub size={20} />
           </a>
-          <a href="https://linkedin.com/in/manishsahanidev" className="mr-9 hover:scale-125 ">
+          <a href="https://linkedin.com/in/manishsahanidev" target='_blank' className="mr-9 hover:scale-125 ">
             <FaLinkedinIn size={20} />
           </a>
-          <a href="mailto:manishsahani7045@gmail.com" className="mr-9 hover:scale-125 ">
+          <a href="mailto:manishsahani7045@gmail.com" target='_blank' className="mr-9 hover:scale-125 ">
             <HiOutlineMail size={20} />
           </a>
         </div>
